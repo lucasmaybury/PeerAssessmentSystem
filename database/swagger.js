73abc65349c2c7ecbb27e46db6fcb419f9b3af71
Lucas Maybury <lmaybury1@sheffield.ac.uk>
2021-04-09 11:34:10 +0100

    add database server (modified copy of api)

---
+++ database/swagger.js
@@ -0,0 +1,17 @@
+const swaggerAutogen = require('swagger-autogen')()
+
+const doc = {
+    info: {
+        title: "Peer Assessment System API",
+        description: "Description"
+    },
+    host: "localhost:3081",
+    schemes: ['http']
+}
+
+const outputFile = './swaggerDocs.json'
+const endpointsFiles = ['./app.js']
+
+swaggerAutogen(outputFile, endpointsFiles, doc).then( () => {
+    require('./bin/www');
+})
