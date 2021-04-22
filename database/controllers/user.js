9b78f78363ab303e5d432dfbb29f383e078a267a
Lucas Maybury <lmaybury1@sheffield.ac.uk>
2021-04-22 12:16:06 +0100

    refactor get functionality for users into SQL query wrapper function

--- database/controllers/user.js
+++ database/controllers/user.js
@@ -1,20 +1,36 @@
 const db = require('../services/db');
 
 exports.getUserByUsername = (req, res) => {
-  const user = {
-    username: 'user1',
-    firstName: 'Lucas',
-    lastName: 'Maybury',
-    role: '2',
-  };
-  res.json(user);
-};
-
-exports.getUsers = (req, res) => {
-  db.query(`SELECT * FROM user`)
+  getUsersFiltered({ id: req.params.username })
     .then(data => res.json(data))
     .catch(err => {
       console.error(err);
       res.send(err);
     });
 };
+
+exports.getUsers = (req, res) => {
+  getUsersFiltered({})
+    .then(data => res.json(data))
+    .catch(err => {
+      console.error(err);
+      res.send(err);
+    });
+};
+
+function getUsersFiltered(filters) {
+  return new Promise(function(resolve, reject) {
+    let whereClauses = '';
+    for (const [key, value] of Object.entries(filters)) {
+      whereClauses += `${key}='${value}'`;
+    }
+    let query = `SELECT * FROM user ${whereClauses != '' ? 'WHERE ' + whereClauses : ''}`;
+    console.log(query);
+    db.query(query)
+      .then(data => {
+        console.log(data);
+        resolve(data);
+      })
+      .catch(err => reject(err));
+  });
+}
