d1e8557fbaa810766937d11366f9c0b9134f00fe
Lucas Maybury <lucas.maybury@3squared.com>
2022-09-23 16:28:29 +0100

    add node server to vue app

---
+++ webapp/server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 8080;
+
+let html = fs.readFileSync('dist/index.html', 'utf-8');
+
+app.get('/index.html', (req, res) => {
+    res.set('Content-Type', 'text/html');
+    res.send(html);
+});
+
+app.use(express.static('./dist'));
+
+app.listen(port);
