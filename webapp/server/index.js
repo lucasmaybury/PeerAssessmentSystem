a02d34b01253efbea6cd4f150df10053d26abca6
Lucas Maybury <lucas.maybury@3squared.com>
2022-10-21 12:23:43 +0100

    fix node app startup

--- webapp/server/index.js
+++ webapp/server/index.js
@@ -5,11 +5,11 @@ const port = 8080;
 
 let html = fs.readFileSync('../dist/index.html', 'utf-8');
 
-app.get('/index.html', (req, res) => {
+app.get('../dist/index.html', (req, res) => {
     res.set('Content-Type', 'text/html');
     res.send(html);
 });
 
-app.use(express.static('./dist'));
+app.use(express.static('../dist'));
 
 app.listen(port);
